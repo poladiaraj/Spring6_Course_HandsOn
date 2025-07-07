2a51430c92a84e9c244a9b8120dbc12160d51fcc
RAJ JAYANT POLADIA <rajpoladia@outlook.com>
2025-07-07 23:47:17 +0530

    Java Projects

---
+++ QuizApp/src/main/java/com/rpoladia/Main.java
@@ -0,0 +1,13 @@
+package com.rpoladia;
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+        QuestionService service = new QuestionService();
+        //service.displayQuestions();
+
+        service.playQuiz();
+        service.printScore();
+    }
+}
